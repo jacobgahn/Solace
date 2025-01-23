73a92548a8e5a6555b97093b82403d17ad0de3b9
jacob.gahn@gmail.com <jacob.gahn@gmail.com>
2025-01-23 11:50:11 -0500

    clear db before seed

--- src/db/seed/seed.ts
+++ src/db/seed/seed.ts
@@ -5,6 +5,7 @@ import { advocateData } from "./advocates";
 
 async function seedDatabase() {
 	try {
+		await db.delete(advocates);
 		const records = await db.insert(advocates).values(advocateData).returning();
 		console.log("Seeded advocates:", records);
 	} catch (error) {
