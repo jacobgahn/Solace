5074c0cf877be849001c9deddb014087aab9bf03
jacob.gahn@gmail.com <jacob.gahn@gmail.com>
2025-01-23 10:54:01 -0500

    Moved seed process from api call to script

---
+++ src/db/seed/seed.ts
@@ -0,0 +1,17 @@
+import "dotenv/config";
+import db from "../../db";
+import { advocates } from "../schema";
+import { advocateData } from "./advocates";
+
+async function seedDatabase() {
+	try {
+		const records = await db.insert(advocates).values(advocateData).returning();
+		console.log("Seeded advocates:", records);
+	} catch (error) {
+		console.error("Error seeding database:", error);
+	} finally {
+		process.exit();
+	}
+}
+
+seedDatabase();
